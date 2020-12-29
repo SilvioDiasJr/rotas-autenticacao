8ea9e408cf645bccd2014348d250d3307b9db65f
Silvio <silvio.dias@etec.sp.gov.br>
2020-12-29 16:08:04 -0300

    criando a página de painel

---
+++ src/pages/Painel/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+function Painel() {
+  return(
+    <div>
+      <h1>Bem vindo você está logado!!</h1>
+    </div>
+  )
+}
+
+export default Painel;
